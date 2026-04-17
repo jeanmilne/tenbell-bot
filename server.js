60acd5e55b990d0202611d70895d86c9a4c4d795
jeanmilne <adamloretoalbini@gmail.com>
2026-04-17 12:08:22 -0400

    changes server.js

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+// Allow requests from anywhere (needed for PWA on phone calling the API)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
